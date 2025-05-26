5191a6a23477188697504625a264bb2a470a185e
Riccardo Bastianini <RBastianini@users.noreply.github.com>
2025-05-26 22:51:30 +0200

    Aggiunto phpunit e mockery
    composer require --dev symfony/test-pack
    composer require --dev mockery/mockery
    composer require --dev phpstan/phpstan-mockery

--- files/.php-cs-fixer.dist.php
+++ files/.php-cs-fixer.dist.php
@@ -8,6 +8,8 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        // Force test method names to use snake case
+        'php_unit_method_casing' => ['case' => 'snake_case'],
     ])
     ->setFinder($finder)
 ;
